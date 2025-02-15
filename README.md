3685756f5f0624ca94eeaa342077cbee9a160057
Ty Piesco <typiesco@gmail.com>
2025-02-15 11:22:21 -0500

    Initial page content

---
+++ README.md
@@ -0,0 +1,1 @@
+Nacho Photo by <a href="https://unsplash.com/@coffeefyworkafe?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Coffeefy Workafe</a> on <a href="https://unsplash.com/photos/black-ceramic-bowl-with-dish-Y0zbn9lPCEU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
--- src/script.js
+++ src/script.js
@@ -1,2 +1,28 @@
 import "./styles.css";
+import nachosImg from "./images/nachos.jpg";
+import reviewStar from "./images/full-star.svg";
+import reviewHalfStar from "./images/half-star.svg";
 console.log("test");
+
+const nachosImgEle = document.createElement("img");
+nachosImgEle.classList.add("food-img");
+nachosImgEle.src = nachosImg;
+
+document.querySelector(".content-1").appendChild(nachosImgEle);
+
+addStars(document.querySelector(".review-stars"), 4.5);
+
+function addStars(ele, numOfStars) {
+    for (let i = 0; i < Math.floor(numOfStars); i++) {
+        const fullStarEle = document.createElement("img");
+        fullStarEle.src = reviewStar;
+        ele.appendChild(fullStarEle);
+    }
+
+    if (numOfStars % 1 == 0.5) {
+        const halfStarEle = document.createElement("img");
+        halfStarEle.src = reviewHalfStar;
+        ele.appendChild(halfStarEle);
+    }
+    return ele;
+}
